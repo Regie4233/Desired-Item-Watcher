e1cfb88e80309b192afa3f2b3cb3a7949ee3da84
Regie4233 <96483953+Regie4233@users.noreply.github.com>
2023-02-08 20:53:30 -0500

    setting interval

--- siteparser.js
+++ siteparser.js
@@ -1,22 +1,33 @@
 const nightmare = require('nightmare')();
 
+let counter = 0;
+
 async function checkPrice() {
-    const priceString = await nightmare
-        .goto("https://www.lenovo.com/us/en/p/laptops/thinkpad/thinkpadx1/x1-extreme-g4/20y5007jus")
-        .wait(".final-price")
-        .evaluate(() =>  document.getElementsByClassName("final-price")[0].innerText)
-        .end();
+    
+    try {
+        const priceString = await nightmare
+            .goto("https://www.lenovo.com/us/en/p/laptops/thinkpad/thinkpadx1/x1-extreme-g4/20y5007jus")
+            .wait(".final-price")
+            .evaluate(() => document.getElementsByClassName("final-price")[0].innerText)
+            .end();
 
-    const price = priceString.replace('$', '').replace(',', '');
+        const price = priceString.replace('$', '').replace(',', '');
 
-    parseFloat(price);
+        parseFloat(price);
 
-    console.log(price);
-    if(price < 1800){
-        console.log('cheaop');
-    }else{
-        console.log('not cheap');
+        console.log(price);
+        if (price < 1800) {
+            console.log('cheaop');
+        } else {
+            console.log('not cheap');
+        }
+    }
+    catch (e) {
+        console.log(e);
     }
 }
 
+setInterval(() => {
+    console.log("!");
+}, 1000);
 checkPrice();
